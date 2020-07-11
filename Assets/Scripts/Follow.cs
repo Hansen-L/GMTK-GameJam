75b1d3aac95eb4a9ce18aedf28369f17eb4cc570
CaiEddie <>
2020-07-11 15:16:50 -0400

    added health bar

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -23,8 +23,19 @@ public class Follow : MonoBehaviour {
         float interpolation = speed * Time.deltaTime;
         
         Vector3 position = this.transform.position;
-        position.x = Mathf.Clamp(Mathf.Lerp(this.transform.position.x, objectToFollow.transform.position.x, interpolation),-boundary_x, boundary_x);
-        position.y = Mathf.Clamp(Mathf.Lerp(this.transform.position.y, objectToFollow.transform.position.y, interpolation), -boundary_y, boundary_y);
+
+		Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 direction = new Vector2 (mousePos.x - objectToFollow.transform.position.x, mousePos.y - objectToFollow.transform.position.y);
+		
+
+        Vector2 newposition = new Vector2(objectToFollow.transform.position.x, objectToFollow.transform.position.y+0.1f);
+
+        direction.Normalize();
+        newposition += direction;
+
+
+        position.x = Mathf.Clamp(Mathf.Lerp(this.transform.position.x, newposition.x, interpolation),-boundary_x, boundary_x);
+        position.y = Mathf.Clamp(Mathf.Lerp(this.transform.position.y, newposition.y, interpolation), -boundary_y, boundary_y);
         
         this.transform.position = position;
     }
