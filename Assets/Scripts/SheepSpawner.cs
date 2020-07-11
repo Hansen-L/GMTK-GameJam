eb83fb937ca43a8d9e147ae7849d55ecb42a6730
Hansen-L <hansen.liu@mail.mcgill.ca>
2020-07-11 07:48:00 -0400

    Sheep tracking lists

--- Assets/Scripts/SheepSpawner.cs
+++ Assets/Scripts/SheepSpawner.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SheepSpawner : MonoBehaviour 
 {
@@ -8,9 +9,26 @@ public class SheepSpawner : MonoBehaviour
     public float spawnPeriod = 4.3f; // Time between spawns
     public GameObject sheepPrefab;
 
+	public List<GameObject> sheepList = new List<GameObject>(); // Store all the spawned sheep. Sheep never die
+	public List<GameObject> panickedSheepList = new List<GameObject>();
+	public List<GameObject> calmSheepList = new List<GameObject>();
+
 	private void Start()
 	{
 		StartCoroutine(SpawnSheep());
+	}
+
+	private void Update()
+	{
+		// Need to reset these lists so we can repopulate them... fuck this is inefficient :')
+		panickedSheepList = new List<GameObject>();
+		calmSheepList = new List<GameObject>();
+
+		foreach (GameObject sheep in sheepList)
+		{
+			if (sheep.GetComponent<SheepController>().isPanicked == true) { panickedSheepList.Add(sheep); }
+			else { calmSheepList.Add(sheep); }
+		}
 	}
 
 	public IEnumerator SpawnSheep()
@@ -18,7 +36,8 @@ public class SheepSpawner : MonoBehaviour
 		while (true)
 		{
 			Vector2 spawnPosition = new Vector2(Random.Range(-spawnRange_x, spawnRange_x), Random.Range(-spawnRange_y, spawnRange_y)); // Choose random position in our designated box
-			Instantiate(sheepPrefab, spawnPosition, Quaternion.identity);
+			GameObject newSheep = Instantiate(sheepPrefab, spawnPosition, Quaternion.identity);
+			sheepList.Add(newSheep);
 
 			yield return new WaitForSeconds(spawnPeriod);
 		}
