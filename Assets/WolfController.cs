2d0aa8daad566b4f69b89f0ae2318f903b0b64be
CaiEddie <>
2020-07-10 22:35:02 -0400

    push sheep prefab

---
+++ Assets/WolfController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class WoldController : MonoBehaviour 
+{
+	
+	Vector2 direction;
+	public float velocity;
+
+	private Rigidbody2D wolfRb;
+	private SpriteRenderer wolfRenderer;
+	
+
+	void Start() 
+	{
+		wolfRb = this.GetComponent<Rigidbody2D>();
+		wolfRenderer = this.GetComponent<SpriteRenderer>();
+	}
+
+	void FixedUpdate()
+	{
+		direction = this.transform.position - GetClosestSheep().transform.position;
+		direction.Normalize();
+
+		wolfRb.velocity = direction*velocity;
+	}
+
+	void Die()
+	{
+
+	}
+
+	public GameObject GetClosestSheep()
+	{
+		GameObject[] sheeps;
+		sheeps = GameObject.FindGameObjectsWithTag("Sheep");
+		GameObject closest = null;
+		float distance = Mathf.Infinity;
+		Vector3 position = transform.position;
+		foreach (GameObject go in sheeps)
+		{
+			Vector3 diff = go.transform.position - position;
+			float curDistance = diff.sqrMagnitude;
+			if (curDistance < distance)
+			{
+				closest = go;
+				distance = curDistance;
+			}
+		}
+		return closest;
+	}
+}
