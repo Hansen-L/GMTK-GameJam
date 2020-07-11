26cbd9670e9e4523c309f3d76934fb36076ff923
Hansen-L <hansen.liu@mail.mcgill.ca>
2020-07-10 21:26:14 -0400

    Fixed boundaries to background stage. Shit might break now

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -6,13 +6,25 @@ public class Follow : MonoBehaviour {
     public GameObject objectToFollow;
     
     public float speed = 2.0f;
-    
-    void Update () {
+
+    private float boundary_x;
+    private float boundary_y;
+
+	private void Start()
+	{
+        GameObject boundaryObj = GameObject.Find("Boundaries");
+        BoundaryNumbers boundary = boundaryObj.GetComponent<BoundaryNumbers>();
+
+        boundary_x = boundary.cameraBoundary_x;
+        boundary_y = boundary.cameraBoundary_y;
+    }
+
+	void Update () {
         float interpolation = speed * Time.deltaTime;
         
         Vector3 position = this.transform.position;
-        position.y = Mathf.Clamp(Mathf.Lerp(this.transform.position.y, objectToFollow.transform.position.y, interpolation), -4, 4);
-        position.x = Mathf.Clamp(Mathf.Lerp(this.transform.position.x, objectToFollow.transform.position.x, interpolation), -2, 2);
+        position.x = Mathf.Clamp(Mathf.Lerp(this.transform.position.x, objectToFollow.transform.position.x, interpolation),-boundary_x, boundary_x);
+        position.y = Mathf.Clamp(Mathf.Lerp(this.transform.position.y, objectToFollow.transform.position.y, interpolation), -boundary_y, boundary_y);
         
         this.transform.position = position;
     }
