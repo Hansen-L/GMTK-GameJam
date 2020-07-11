b09a146c8245142b247db55fe30007ab4123889d
CaiEddie <>
2020-07-11 16:41:27 -0400

    added stages

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,17 +5,31 @@ public class GameManager : MonoBehaviour
     public int gameOverPercent = 90;
     public SheepPercentHUD sheepPrecentHUD;
     public GameObject gameOverPrefab;
+    public GameObject stagePrefab;
+    public int stage;
 
+    private float timer;
     private bool gameEnded = false;
 
     void Start() 
     {
         sheepPrecentHUD = GameObject.Find("Percent").GetComponent<SheepPercentHUD>();
+        timer = 0;
+        stage = 1;
     }
 
 
     void Update() 
     {
+     	timer += Time.deltaTime;   
+    	if (timer > 10){
+    		GameObject stageComplete = Instantiate(stagePrefab, new Vector3(0, 0), Quaternion.identity);
+    		stage +=1;
+    		timer = 0;
+    		Time.timeScale = 0;
+    	}
+
+
         if (sheepPrecentHUD.percentPanicked > gameOverPercent && !gameEnded)
         {
             Instantiate(gameOverPrefab, new Vector3(0, 0), Quaternion.identity);
---
+++ Assets/StageScreenScript.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageScreenScript : MonoBehaviour
+{
+
+
+	public GameObject gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("Complete").GetComponent<TextMeshProUGUI>().text = "STAGE " + gameManager.GetComponent<GameManager>().stage + " COMPLETE";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
